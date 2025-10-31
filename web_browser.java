0e5a35dd0c2a3d8dd26e9919348aa591ff667a7b
pulkitsinha <pulkitOFFICIALsinha@gmail.com>
2025-10-31 06:10:09 +0530

    Fix formatting and remove unnecessary lines

--- web_browser.java
+++ web_browser.java
@@ -6,6 +6,8 @@ public class web_browser {
 	public static void main(String[] args){
 		ReadFile akhirHoGya= new ReadFile();
 		akhirHoGya.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}}
+	}
+}
+
 
 
